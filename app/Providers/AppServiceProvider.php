4356194414b9707fd0570a949d24f17334ca0e0e
Felipe Pena <>
2020-11-14 01:30:48 -0300

    minor ssl fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      if(env('APP_ENV') === 'prod') {
+        \URL::forceScheme('https');
+      }
+
       \Schema::defaultStringLength(191);
     }
 }
